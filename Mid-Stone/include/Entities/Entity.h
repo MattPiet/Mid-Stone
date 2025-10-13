427bd31d4a923aa35d4bf263b5a1478740feb534
Sebastian Villarroel <sebastian.gvg94@gmail.com>
2025-10-13 18:39:33 -0400

    merging Entity and Player into master
    * feat: add player and entity
    
    * feat: upload images
    
    * feat: Added functions for linear motion, also made files propely positioned Finally fixed a bug in the include directories
    
    ---------
    
    Co-authored-by: Sebastian-V-Apply-Digital <sebastian.villarroel@applydigital.com>
    Co-authored-by: matthew pietrafesa <mattpietrafesa@gmail.com>

---
+++ Mid-Stone/include/Entities/Entity.h
@@ -0,0 +1,78 @@
+﻿#pragma once
+#include <functional>
+#include <Matrix.h>
+#include <Quaternion.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+class Entity
+{
+private:
+    
+
+    MATH::Vec3 scale;
+    MATH::Vec3 position;
+    MATH::Vec3 velocity;
+    MATH::Vec3 acceleration;
+	float mass{ 1.0f };
+    
+
+    using ExpiredCallback = std::function<void(Entity&)>;
+    /**
+     * Callback for a function that will be executed once this entity expires
+     */
+    ExpiredCallback onExpired;
+    
+    /**
+     * Life span for the entity, if it's 0, the object is immortal, if not, it will be the time in
+     * seconds that the entity will live before destructing itself.
+     */
+    float lifeSpanSeconds{0.0f};
+
+    /**
+     * Counter of seconds to calculate the lifespan of this entity.
+     */
+    float currentLifeTimeSeconds{0.0f};
+    bool hasFiredExpiredHooks{false};
+
+protected:
+
+    MATH::Quaternion orientation;
+    
+    virtual void OnExpired()
+    {
+        std::cout << "Entity expired" << std::endl;
+    }
+
+public:
+    Entity() = default;
+    Entity(const MATH::Vec3& position, const MATH::Vec3& scale);
+
+    virtual ~Entity()
+    {
+        std::cout << "Entity destroyed" << std::endl;
+    };
+    
+    void SetLifeSpan(float lifeSpanSeconds);
+    [[nodiscard]] float GetLifeSpan() const { return lifeSpanSeconds; }
+    [[nodiscard]] float GetCurrentLifeTime() const { return currentLifeTimeSeconds; }
+    void SetExpiredCallback(ExpiredCallback callback) { onExpired = std::move(callback); }
+    
+    /**
+     * Checks whether the current lifetime surpasses the expected lifespan, as long as the lifespan is > 0
+     * @return boolean indicating whether it is expired or not
+     */
+    [[nodiscard]] bool IsExpired() const;
+    [[nodiscard]] bool IsImmortal() const { return lifeSpanSeconds <= 0.0f; }
+
+
+    [[nodiscard]] MATH::Vec3 GetPosition() const { return position; }
+    [[nodiscard]] MATH::Vec3 GetScale() const { return scale; }
+
+    [[nodiscard]] MATH::Matrix4 GetModelMatrix() const;
+
+    void Update(float deltaTime);
+
+    void ApplyForce(MATH::Vec3 force);
+    void UpdatePosition(float deltaTime);
+};
--- Mid-Stone/src/Scenes/Scene0g.cpp
+++ Mid-Stone/src/Scenes/Scene0g.cpp
@@ -11,17 +11,20 @@
 #include <Graphics/SpriteMesh.h>
 #include <SDL_mixer.h>
 #include <Graphics/SpriteRenderer.h>
+#include <glm/gtx/string_cast.hpp>
+
 
 ///ImGui includes
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 
-//#include <Utils/MemoryMonitor.h>
+#include <Utils/MemoryMonitor.h>
 
-Scene0g::Scene0g() : sphere{ nullptr }, shader{ nullptr }, mesh{ nullptr },
-drawInWireMode{ false }, master_volume{ 1.0f }, mixer{ nullptr }, sprite_Mesh{ nullptr }, sprite_Renderer{ nullptr }, 
-spriteSheet_Renderer{ nullptr }
+Scene0g::Scene0g() : sphere{nullptr}, shader{nullptr}, mesh{nullptr},
+                     drawInWireMode{false}, master_volume{1.0f}, mixer{nullptr}, sprite_Mesh{nullptr},
+                     sprite_Renderer{nullptr},
+                     spriteSheet_Renderer{nullptr}
 {
     Debug::Info("Created Scene0: ", __FILE__, __LINE__);
 }
@@ -40,15 +43,15 @@ bool Scene0g::OnCreate()
     mesh = new Mesh("meshes/Sphere.obj");
     mesh->OnCreate();
 
-	// Create the sprite mesh and sprite renderer //// look in the headr file for more info
+    // Create the sprite mesh and sprite renderer //// look in the headr file for more info
     sprite_Mesh = new SpriteMesh();
     sprite_Mesh->OnCreate();
 
     sprite_Renderer = new SpriteRenderer();
-    sprite_Renderer->loadImage("sprites/fatty_clicked.png");
+    sprite_Renderer->loadImage("sprites/fist.png", 2, 8);
 
-	spriteSheet_Renderer = new SpriteRenderer();
-	spriteSheet_Renderer->loadImage("sprites/idle.png", 1, 3); 
+    spriteSheet_Renderer = new SpriteRenderer();
+    spriteSheet_Renderer->loadImage("sprites/idle.png", 1, 3);
 
     shader = new Shader("shaders/spriteVert.glsl", "shaders/spriteFrag.glsl");
     if (!shader->OnCreate())
@@ -57,19 +60,19 @@ bool Scene0g::OnCreate()
     }
 
     projectionMatrix = MMath::perspective(45.0f, (16.0f / 9.0f), 0.5f, 100.0f);
- 
-	/// Set up the sprite projection matrix //// This is orthographic for 2D rendering
+
+    /// Set up the sprite projection matrix //// This is orthographic for 2D rendering
     spriteProjectionMatrix = MMath::orthographic(0.0f, 1280.0f / 10.0f,
                                                  0.0f, 720.0f / 10.0f,
                                                  -1.0f, 1.0f);
 
     viewMatrix = MMath::lookAt(Vec3(0.0f, 0.0f, 5.0f), Vec3(0.0f, 0.0f, 0.0f), Vec3(0.0f, 1.0f, 0.0f));
     modelMatrix.loadIdentity();
-	/// Move the sprite to the center of the screen ish
-    modelMatrix = MMath::translate(44.0f,36.0f,0.0f);
+    /// Move the sprite to the center of the screen ish
+    modelMatrix = MMath::translate(44.0f, 36.0f, 0.0f);
 
     spriteSheet_ModelMatrix.loadIdentity();
-	spriteSheet_ModelMatrix = MMath::translate(84.0f, 36.0f, 0.0f);
+    spriteSheet_ModelMatrix = MMath::translate(84.0f, 36.0f, 0.0f);
 
     SDL_Init(SDL_INIT_AUDIO);
     MIX_Init();
@@ -104,6 +107,18 @@ bool Scene0g::OnCreate()
     }*/
 
 
+    /** Renderer Setup **/
+    crossHairsRenderer = new SpriteRenderer();
+    crossHairsRenderer->loadImage("sprites/crosshairs.png");
+    playerRenderer = new SpriteRenderer();
+    playerRenderer->loadImage("sprites/idle.png", 1, 3);
+    bulletsRenderer = new SpriteRenderer();
+    bulletsRenderer->loadImage("sprites/fist.png", 2, 8);
+    impactRenderer = new SpriteRenderer();
+    impactRenderer->loadImage("sprites/impact.png", 2, 4);
+    players.emplace_back(std::make_unique<Player>(Vec3{15, 15, 0}, Vec3{1.5f, 1.5f, 1.5f}));
+
+
     return true;
 }
 
@@ -126,11 +141,23 @@ void Scene0g::OnDestroy()
     delete sprite_Mesh;
     sprite_Mesh = nullptr;
 
-	delete sprite_Renderer;
+    delete sprite_Renderer;
     sprite_Renderer = nullptr;
 
-	delete spriteSheet_Renderer;
-	spriteSheet_Renderer = nullptr;
+    delete spriteSheet_Renderer;
+    spriteSheet_Renderer = nullptr;
+
+    delete playerRenderer;
+    playerRenderer = nullptr;
+
+    delete bulletsRenderer;
+    bulletsRenderer = nullptr;
+
+    delete crossHairsRenderer;
+    crossHairsRenderer = nullptr;
+
+    delete impactRenderer;
+    impactRenderer = nullptr;
 
     //// Turn off audio
     if (mixer)
@@ -149,6 +176,40 @@ void Scene0g::HandleEvents(const SDL_Event& sdlEvent)
         {
         case SDL_SCANCODE_W:
             drawInWireMode = !drawInWireMode;
+            break;
+        case SDL_SCANCODE_A:
+            pressingLeft = true;
+            break;
+        case SDL_SCANCODE_D:
+            pressingRight = true;
+            break;
+        case SDL_SCANCODE_SPACE:
+            if (auto bullet = players.front()->Shoot())
+            {
+                // implicit upcast unique_ptr<Bullet> -> unique_ptr<Entity> (default deleter)
+                bullet->SetExpiredCallback([this](Entity& e)
+                {
+                    auto impact = std::make_unique<Entity>(e.GetPosition(), Vec3{1.0f, 1.0f, 1.0f});
+                    impact->SetLifeSpan(1.0f);
+                    effects.emplace_back(std::move(impact));
+                });
+                bullets.emplace_back(std::move(bullet));
+            }
+            break;
+        }
+        break;
+
+    case SDL_EVENT_KEY_UP:
+        switch (sdlEvent.key.scancode)
+        {
+        case SDL_SCANCODE_W:
+            drawInWireMode = !drawInWireMode;
+            break;
+        case SDL_SCANCODE_A:
+            pressingLeft = false;
+            break;
+        case SDL_SCANCODE_D:
+            pressingRight = false;
             break;
         }
         break;
@@ -194,8 +255,54 @@ void Scene0g::RenderGUI()
     ImGui::End();
 }
 
-void Scene0g::Update(const float deltaTime){
+void Scene0g::Update(const float deltaTime)
+{
+    /** Update Players **/
+    if (pressingLeft && !pressingRight)
+    {
+        players.front()->MoveAim(2.0f);
+    }
+    if (pressingRight && !pressingLeft)
+    {
+        players.front()->MoveAim(-2.0f);
+    }
 
+    /* Regular Loop for Updating Players */
+    for (auto& player : players)
+    {
+        player->Update(deltaTime);
+    }
+    /* Regular Loop for Updating Bullets */
+    for (auto& bullet : bullets)
+    {
+        bullet->Update(deltaTime);
+    }
+    /* Regular Loop for Updating Effects */
+    for (auto& effect : effects)
+    {
+        effect->Update(deltaTime);
+    }
+
+    /* Removes and Deletes Players if they expire */
+    players.erase(
+        std::remove_if(players.begin(), players.end(),
+                       [](const std::unique_ptr<Player>& e) { return e->IsExpired(); }),
+        players.end()
+    );
+
+    /* Removes and Deletes bullets if they expire */
+    bullets.erase(
+        std::remove_if(bullets.begin(), bullets.end(),
+                       [](const std::unique_ptr<Entity>& e) { return e->IsExpired(); }),
+        bullets.end()
+    );
+
+    /* Removes and Deletes effects if they expire */
+    effects.erase(
+        std::remove_if(effects.begin(), effects.end(),
+                       [](const std::unique_ptr<Entity>& e) { return e->IsExpired(); }),
+        effects.end()
+    );
 }
 
 void Scene0g::Render() const
@@ -218,24 +325,34 @@ void Scene0g::Render() const
 
     glUseProgram(shader->GetProgram());
     glUniformMatrix4fv(shader->GetUniformID("projectionMatrix"), 1, GL_FALSE, spriteProjectionMatrix);
-	//// Render the sprite
-	// no but like actually this is all you need in the scene render function to render a sprite
-	// look at the header file for more info but basically you need a sprite mesh and a sprite renderer
-	// to animate a sprite sheet just pass in the current sprite index to the renderSprite function
-	// so we still need to figure out how to animate the sprite sheet
+    //// Render the sprite
+    // no but like actually this is all you need in the scene render function to render a sprite
+    // look at the header file for more info but basically you need a sprite mesh and a sprite renderer
+    // to animate a sprite sheet just pass in the current sprite index to the renderSprite function
+    // so we still need to figure out how to animate the sprite sheet
 
-	sprite_Renderer->renderSprite(shader, sprite_Mesh, modelMatrix); // current_sprite_index
+    sprite_Renderer->renderSprite(shader, sprite_Mesh, modelMatrix); // current_sprite_index
 
-	spriteSheet_Renderer->renderSprite(shader, sprite_Mesh, spriteSheet_ModelMatrix, 0); // current_sprite_index
+    /* Regular Loop for Rendering Players */
+    for (auto& player : players)
+    {
+        playerRenderer->renderSprite(shader, sprite_Mesh, player->GetModelMatrix());
+        crossHairsRenderer->renderSprite(shader, sprite_Mesh, player->GetAimModelMatrix());
+    }
+
+    /* Regular Loop for Rendering Bullets */
+    for (auto& bullet : bullets)
+    {
+        bulletsRenderer->renderSprite(shader, sprite_Mesh, bullet->GetModelMatrix());
+    }
+
+    /* Regular Loop for Rendering Effects */
+    for (auto& effect : effects)
+    {
+        impactRenderer->renderSprite(shader, sprite_Mesh, effect->GetModelMatrix());
+    }
+
+    spriteSheet_Renderer->renderSprite(shader, sprite_Mesh, spriteSheet_ModelMatrix, 0); // current_sprite_index
 
     glUseProgram(0);
-
-
-
-
 }
-
-
-
-
-	
