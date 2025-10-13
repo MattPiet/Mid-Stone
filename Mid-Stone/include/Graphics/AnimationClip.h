919a42dad349ff8eb9b52fe4b4f218e04c9f6f8f
lavalinx-11 <zahirlawrence2@gmail.com>
2025-10-13 17:35:51 -0400

    Feat: Setup AnimationClip and Animator classes

--- Mid-Stone/include/Graphics/AnimationClip.h
+++ Mid-Stone/include/Graphics/AnimationClip.h
@@ -1,6 +1,34 @@
 #pragma once
 class AnimationClip{
+public:
+	enum class PlayMode {
+		ONCE,
+		LOOP,
+		PINGPONG,
+		REVERSE
+	};
+private:
+	//Duration of each frame in seconds
+	float frameDuration;
+	//the playback mode of the animation clip (determines if it loops or plays once etcP)
+	PlayMode playMode;
+	//the current frame in the clip
+	int currentFrame;
+	//the total number of frames in the clip
+	int totalFrames;
+public:
+	//Default constructor which sets the frame duration to 0.1 with a default playmode to loop
+	AnimationClip() : frameDuration{ 0.1f }, playMode{ PlayMode::LOOP }, currentFrame{ 0 } {}
+	
+	//Dynamic constructor to fill in values 
+	AnimationClip(float frameDuration_, PlayMode playMode_): frameDuration{ frameDuration_ }, playMode{ playMode_ } {}
+	
+	//Basic getter to retrieve the frame duration
+	float getFrameDuration() const { return frameDuration; }
 
 
+	//Basic getter to retrieve the playback mode of the animation
+	PlayMode getPlayMode() const { return playMode; }
+
+	void resetFrame() { currentFrame = 0; }
 };
-
--- Mid-Stone/include/Graphics/Animator.h
+++ Mid-Stone/include/Graphics/Animator.h
@@ -1,6 +1,48 @@
 #pragma once
+#include "AnimationClip.h"
+#include <string>
+#include <unordered_map>
+#include <iostream>
+using namespace std;
 class Animator{
+private:
+	//The name of the current clip being played
+	string currentClipName;
+
+	//Pointer to the current clip being played
+	AnimationClip* currentClip;
+
+	//Holds the animation clips in an unordered map for easy access via a name that is tied to the animation clip
+	std::unordered_map<string, AnimationClip*> animationClips;
+public:
+
+	//Basic getter to retrieve the current clips name
+	string getCurrentClipName() const { return currentClipName; }
 
 
+	//Adds a new Animation clip to the animator using emplace (emplace adds new data into the map)
+	void addAnimationClip(const string& name, AnimationClip* clip) {animationClips.emplace(name, clip);}
+
+
+	//Plays an animation clip based off a name provided
+	void playAnimationClip(const string& name) {
+
+		//Automatically iterates through the map to find the clip
+		auto it = animationClips.find(name);
+
+		//If found set the current clip to the found clip
+		if (it != animationClips.end()) {
+			currentClipName = name;
+			currentClip = it->second;
+			currentClip->resetFrame();
+		}
+		//If the clip is not found set the current clip to null and the name to an empty string and print that there was no clip found
+		else {
+			currentClipName = "";
+			currentClip = nullptr;
+			cout << "Clip not found!" << endl;
+			throw runtime_error("Animation clip'" + name + "'not found.");
+		} 
+	}
 };
 
